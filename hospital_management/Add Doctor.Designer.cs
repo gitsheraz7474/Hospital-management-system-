d8e5c6154e98bab4476a9b48a60acb13a79fbe8b
sheraz ibrahim <sherazibrahim774@gmail.com>
2023-05-27 05:18:56 -0700

    folder added

--- hospital_management/Add Doctor.Designer.cs
+++ hospital_management/Add Doctor.Designer.cs
@@ -28,12 +28,204 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rdbFemale = new System.Windows.Forms.RadioButton();
+            this.rdbMale = new System.Windows.Forms.RadioButton();
+            this.lblDoctorAddress = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtDoctorAddress = new System.Windows.Forms.TextBox();
+            this.txtDoctorContact = new System.Windows.Forms.TextBox();
+            this.txtDoctorSalary = new System.Windows.Forms.TextBox();
+            this.txtDoctorName = new System.Windows.Forms.TextBox();
+            this.txtDoctorID = new System.Windows.Forms.TextBox();
+            this.lblDoctorContact = new System.Windows.Forms.Label();
+            this.lblDoctorSalary = new System.Windows.Forms.Label();
+            this.lblDoctorGender = new System.Windows.Forms.Label();
+            this.lblDoctorName = new System.Windows.Forms.Label();
+            this.lblDoctorID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rdbFemale
+            // 
+            this.rdbFemale.AutoSize = true;
+            this.rdbFemale.Location = new System.Drawing.Point(372, 289);
+            this.rdbFemale.Name = "rdbFemale";
+            this.rdbFemale.Size = new System.Drawing.Size(59, 17);
+            this.rdbFemale.TabIndex = 36;
+            this.rdbFemale.TabStop = true;
+            this.rdbFemale.Text = "Female";
+            this.rdbFemale.UseVisualStyleBackColor = true;
+            // 
+            // rdbMale
+            // 
+            this.rdbMale.AutoSize = true;
+            this.rdbMale.Location = new System.Drawing.Point(279, 289);
+            this.rdbMale.Name = "rdbMale";
+            this.rdbMale.Size = new System.Drawing.Size(48, 17);
+            this.rdbMale.TabIndex = 35;
+            this.rdbMale.TabStop = true;
+            this.rdbMale.Text = "Male";
+            this.rdbMale.UseVisualStyleBackColor = true;
+            // 
+            // lblDoctorAddress
+            // 
+            this.lblDoctorAddress.AutoSize = true;
+            this.lblDoctorAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblDoctorAddress.Location = new System.Drawing.Point(569, 286);
+            this.lblDoctorAddress.Name = "lblDoctorAddress";
+            this.lblDoctorAddress.Size = new System.Drawing.Size(120, 20);
+            this.lblDoctorAddress.TabIndex = 34;
+            this.lblDoctorAddress.Text = "Doctor Address";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(458, 398);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(145, 40);
+            this.btnAdd.TabIndex = 33;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // txtDoctorAddress
+            // 
+            this.txtDoctorAddress.Location = new System.Drawing.Point(702, 286);
+            this.txtDoctorAddress.Name = "txtDoctorAddress";
+            this.txtDoctorAddress.Size = new System.Drawing.Size(168, 20);
+            this.txtDoctorAddress.TabIndex = 32;
+            // 
+            // txtDoctorContact
+            // 
+            this.txtDoctorContact.Location = new System.Drawing.Point(702, 222);
+            this.txtDoctorContact.Name = "txtDoctorContact";
+            this.txtDoctorContact.Size = new System.Drawing.Size(168, 20);
+            this.txtDoctorContact.TabIndex = 31;
+            // 
+            // txtDoctorSalary
+            // 
+            this.txtDoctorSalary.Location = new System.Drawing.Point(702, 165);
+            this.txtDoctorSalary.Name = "txtDoctorSalary";
+            this.txtDoctorSalary.Size = new System.Drawing.Size(168, 20);
+            this.txtDoctorSalary.TabIndex = 30;
+            // 
+            // txtDoctorName
+            // 
+            this.txtDoctorName.Location = new System.Drawing.Point(279, 220);
+            this.txtDoctorName.Name = "txtDoctorName";
+            this.txtDoctorName.Size = new System.Drawing.Size(152, 20);
+            this.txtDoctorName.TabIndex = 28;
+            // 
+            // txtDoctorID
+            // 
+            this.txtDoctorID.Location = new System.Drawing.Point(279, 163);
+            this.txtDoctorID.Name = "txtDoctorID";
+            this.txtDoctorID.Size = new System.Drawing.Size(152, 20);
+            this.txtDoctorID.TabIndex = 27;
+            // 
+            // lblDoctorContact
+            // 
+            this.lblDoctorContact.AutoSize = true;
+            this.lblDoctorContact.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblDoctorContact.Location = new System.Drawing.Point(569, 220);
+            this.lblDoctorContact.Name = "lblDoctorContact";
+            this.lblDoctorContact.Size = new System.Drawing.Size(117, 20);
+            this.lblDoctorContact.TabIndex = 26;
+            this.lblDoctorContact.Text = "Doctor Contact";
+            // 
+            // lblDoctorSalary
+            // 
+            this.lblDoctorSalary.AutoSize = true;
+            this.lblDoctorSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblDoctorSalary.Location = new System.Drawing.Point(569, 161);
+            this.lblDoctorSalary.Name = "lblDoctorSalary";
+            this.lblDoctorSalary.Size = new System.Drawing.Size(97, 20);
+            this.lblDoctorSalary.TabIndex = 25;
+            this.lblDoctorSalary.Text = "Dotor Salary";
+            // 
+            // lblDoctorGender
+            // 
+            this.lblDoctorGender.AutoSize = true;
+            this.lblDoctorGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblDoctorGender.Location = new System.Drawing.Point(130, 286);
+            this.lblDoctorGender.Name = "lblDoctorGender";
+            this.lblDoctorGender.Size = new System.Drawing.Size(115, 20);
+            this.lblDoctorGender.TabIndex = 23;
+            this.lblDoctorGender.Text = "Doctor Gender";
+            // 
+            // lblDoctorName
+            // 
+            this.lblDoctorName.AutoSize = true;
+            this.lblDoctorName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblDoctorName.Location = new System.Drawing.Point(130, 220);
+            this.lblDoctorName.Name = "lblDoctorName";
+            this.lblDoctorName.Size = new System.Drawing.Size(103, 20);
+            this.lblDoctorName.TabIndex = 22;
+            this.lblDoctorName.Text = "Doctor Name";
+            // 
+            // lblDoctorID
+            // 
+            this.lblDoctorID.AutoSize = true;
+            this.lblDoctorID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblDoctorID.Location = new System.Drawing.Point(130, 163);
+            this.lblDoctorID.Name = "lblDoctorID";
+            this.lblDoctorID.Size = new System.Drawing.Size(78, 20);
+            this.lblDoctorID.TabIndex = 21;
+            this.lblDoctorID.Text = "Doctor ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(452, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 31);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "Add Doctor";
+            // 
+            // Add_Doctor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1001, 596);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rdbFemale);
+            this.Controls.Add(this.rdbMale);
+            this.Controls.Add(this.lblDoctorAddress);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtDoctorAddress);
+            this.Controls.Add(this.txtDoctorContact);
+            this.Controls.Add(this.txtDoctorSalary);
+            this.Controls.Add(this.txtDoctorName);
+            this.Controls.Add(this.txtDoctorID);
+            this.Controls.Add(this.lblDoctorContact);
+            this.Controls.Add(this.lblDoctorSalary);
+            this.Controls.Add(this.lblDoctorGender);
+            this.Controls.Add(this.lblDoctorName);
+            this.Controls.Add(this.lblDoctorID);
+            this.Name = "Add_Doctor";
             this.Text = "Add_Doctor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton rdbFemale;
+        private System.Windows.Forms.RadioButton rdbMale;
+        private System.Windows.Forms.Label lblDoctorAddress;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtDoctorAddress;
+        private System.Windows.Forms.TextBox txtDoctorContact;
+        private System.Windows.Forms.TextBox txtDoctorSalary;
+        private System.Windows.Forms.TextBox txtDoctorName;
+        private System.Windows.Forms.TextBox txtDoctorID;
+        private System.Windows.Forms.Label lblDoctorContact;
+        private System.Windows.Forms.Label lblDoctorSalary;
+        private System.Windows.Forms.Label lblDoctorGender;
+        private System.Windows.Forms.Label lblDoctorName;
+        private System.Windows.Forms.Label lblDoctorID;
+        private System.Windows.Forms.Label label1;
     }
 }
--- hospital_management/View_Staff_Information.Designer.cs
+++ hospital_management/View_Staff_Information.Designer.cs
@@ -30,53 +30,59 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.lblStaffID = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.bntView = new System.Windows.Forms.Button();
+            this.bntSearchByID = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dgvViewStaffInfo = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvViewStaffInfo)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(336, 71);
+            this.label1.Location = new System.Drawing.Point(370, 47);
             this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(242, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "View Staff Information";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // lblStaffID
             // 
             this.lblStaffID.AutoSize = true;
-            this.lblStaffID.Location = new System.Drawing.Point(128, 140);
+            this.lblStaffID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStaffID.Location = new System.Drawing.Point(262, 135);
             this.lblStaffID.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblStaffID.Name = "lblStaffID";
-            this.lblStaffID.Size = new System.Drawing.Size(90, 25);
+            this.lblStaffID.Size = new System.Drawing.Size(73, 20);
             this.lblStaffID.TabIndex = 1;
             this.lblStaffID.Text = "Staff ID";
             // 
-            // button1
+            // bntView
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(394, 191);
-            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 44);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.bntView.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntView.Location = new System.Drawing.Point(375, 211);
+            this.bntView.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.bntView.Name = "bntView";
+            this.bntView.Size = new System.Drawing.Size(127, 36);
+            this.bntView.TabIndex = 2;
+            this.bntView.Text = "View";
+            this.bntView.UseVisualStyleBackColor = true;
+            this.bntView.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // bntSearchByID
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(623, 191);
-            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(162, 44);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.bntSearchByID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntSearchByID.Location = new System.Drawing.Point(545, 211);
+            this.bntSearchByID.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.bntSearchByID.Name = "bntSearchByID";
+            this.bntSearchByID.Size = new System.Drawing.Size(148, 35);
+            this.bntSearchByID.TabIndex = 3;
+            this.bntSearchByID.Text = "Search  By ID";
+            this.bntSearchByID.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
@@ -87,15 +93,32 @@
             this.dataGridView1.Size = new System.Drawing.Size(520, 288);
             this.dataGridView1.TabIndex = 4;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(394, 128);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 31);
+            this.textBox1.TabIndex = 5;
+            // 
+            // dgvViewStaffInfo
+            // 
+            this.dgvViewStaffInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvViewStaffInfo.Location = new System.Drawing.Point(12, 268);
+            this.dgvViewStaffInfo.Name = "dgvViewStaffInfo";
+            this.dgvViewStaffInfo.Size = new System.Drawing.Size(988, 270);
+            this.dgvViewStaffInfo.TabIndex = 6;
+            // 
             // View_Staff_Information
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1001, 598);
+            this.Controls.Add(this.dgvViewStaffInfo);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bntSearchByID);
+            this.Controls.Add(this.bntView);
             this.Controls.Add(this.lblStaffID);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -103,6 +126,7 @@
             this.Name = "View_Staff_Information";
             this.Text = "View_Staff_Information";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvViewStaffInfo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,8 +136,10 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblStaffID;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button bntView;
+        private System.Windows.Forms.Button bntSearchByID;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dgvViewStaffInfo;
     }
 }
